99a0513f374635ff9c340caad30b6451825c2a20
Katie Eells <katieeells@Katies-MBP.lan>
2023-01-21 15:19:20 -0500

    started bootstrap styling and picking colors

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// Modules & Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settigns
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -15,4 +20,5 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// PORT connections
 app.listen(process.env.PORT)
