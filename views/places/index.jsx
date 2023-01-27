b6530707986dbc4dc1c15200032dc0a5ca6cfa37
Katie Eells <katieeells@Katies-MBP.lan>
2023-01-26 20:55:02 -0500

    connected show route

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,7 +5,11 @@ function index (data) {
     let placesFormatted = data.places.map((place, index) => {
         return (
             <div className="col-sm-6">
-                <h2>{place.name}</h2>
+                <h2>
+                    <a className='placeName' href={`/places/${index}`} >
+                        {place.name}
+                    </a>
+                </h2>
                 <p className='text-center'>
                     {place.cuisines}
                 </p>
