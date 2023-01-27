b95d5de026c44d6a82bc35c54ed90f04e64118f0
Katie Eells <katieeells@Katies-MBP.lan>
2023-01-26 20:19:10 -0500

    recode some data about new places

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,24 @@
 const router = require('express').Router()
+const places = require('../models/places')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'Busters on 28th',
-        city: 'Minneapolis',
-        state: 'MN',
-        cuisines: 'Beer bar, American',
-        pic: 'https://images.unsplash.com/photo-1546622891-02c72c1537b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmVlciUyMHRhcHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
-    }, {
-        name: 'Brewery Bhavana',
-        city: 'Raleigh',
-        state: 'NC',
-        cuisines: 'Brewery, Chinese',
-        pic: 'https://images.unsplash.com/photo-1518983546435-91f8b87fe561?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGltJTIwc3VtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-    }]
     res.render('places/index', { places })
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
-})
+    if (!req.body.pic) {
+        req.body.pic = 'https://images.unsplash.com/photo-1571974448718-ac26a9af7d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3RvcmVmcm9udHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+    })
+
 
 router.get('/new', (req, res) => {
     res.render('places/new')
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Busters on 28th',
+    city: 'Minneapolis',
+    state: 'MN',
+    cuisines: 'Beer bar, American',
+    pic: 'https://images.unsplash.com/photo-1546622891-02c72c1537b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmVlciUyMHRhcHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+}, {
+    name: 'Brewery Bhavana',
+    city: 'Raleigh',
+    state: 'NC',
+    cuisines: 'Brewery, Chinese',
+    pic: 'https://images.unsplash.com/photo-1518983546435-91f8b87fe561?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGltJTIwc3VtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+}]
