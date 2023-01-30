bf6f367aad6e2002d3d4a3675b10b0654b1f5898
Katie Eells <katieeells@Katies-MBP.lan>
2023-01-29 21:16:00 -0500

    started styling the show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,27 @@
 const React = require('react')
 const Def = require('../default')
 
-function showPlace () {
+function showPlace (data) {
     return (
         <Def>
             <main>
-                <h1>DETAILS OF A PLACE</h1>
-                <p>This form will show details of the places</p>
+                <h1>{ data.place.name }</h1>
+                <img src={ data.place.pic } alt={ data.place.name }/>
+                <h3>Rating</h3>
+                <p>None yet</p>
+                <h3>Description</h3>
+                <p className='text-center'>
+                    Located in { data.place.city }, { data.place.state } and serving { data.place.cuisines }.
+                </p>
+                <p>Future comments section</p>
+                <a href={`/places/${data.i}/edit`} className="btn btn-warning"> 
+                    Edit   
+				</a>
+				<form method="POST" action={`/places/${data.i}?_method=DELETE`}> 
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
             </main>
         </Def>
     )
